db5fbf6a4faa7465b1f73895cc4d9b04d070c5ab
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-30 14:01:52 +0530

    Added: UC10_JunitTest_To_ValidateUser

---
+++ Day-24-LambdaExp-UserRegistration/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_24_LambdaExp_UserRegistration;
+
+Console.WriteLine("------ Welcome To USer Validation System ------\n");
+UserValidation.User_Validation(); 
